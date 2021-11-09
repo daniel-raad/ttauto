723d2d948dd75a2d4d5ebed989da75b416878eb7
Daniel Raad <daniel_raad@hotmail.co.uk>
2021-11-09 20:17:16 +0000

    deleting prints

--- download.py
+++ download.py
@@ -12,17 +12,15 @@ def filter_by_tag(tag, number_of_history, number_of_most_liked):
 
 if __name__ == '__main__':
     api = TikTokApi.get_instance()
-    print(api)
     username = "emilymariko"
-    # This is generating the tt_webid_v2 cookie
-    # need to pass it to methods you want to download
-    device_id = api.generate_device_id()
 
+    device_id = api.generate_device_id()
     user_videos = api.by_username(username, 10)
+
     like_list = [] 
     for video in user_videos:
         like_list.append(video['stats']['diggCount'])
-    print(like_list)
+
     enumerate_object = enumerate(like_list) 
     sorted_pairs = sorted(enumerate_object, key=operator.itemgetter(1), reverse=True)
     index_list = [index for index, element in sorted_pairs]
