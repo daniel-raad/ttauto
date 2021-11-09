86ba8d2e7e4cc3ab03712ffc6ef0f98c00c819fa
Daniel Raad <dr1g18@soton.ac.uk>
2021-11-09 20:08:19 +0000

    can now filter by most liked tik tok for a user

--- download.py
+++ download.py
@@ -1,19 +1,49 @@
 # Input a hard coded tag and take the first 10 videos from TikTok 
 from TikTokApi import TikTokApi
 from datetime import datetime 
+import json
+import operator
 
 if __name__ == '__main__':
     api = TikTokApi.get_instance()
     print(api)
-    username = "therock"
+    username = "emilymariko"
     # This is generating the tt_webid_v2 cookie
     # need to pass it to methods you want to download
     device_id = api.generate_device_id()
 
+
+
+    ## filter based on the most liked videos
+    ## grab all the videos by username and then check the most liked videos
+
+
     # trending = api.by_trending(custom_device_id=device_id)
     user_videos = api.by_username(username, 10)
-    # Below is if the method used if you have the full tiktok object
-    for i in range(1,4):
-        video_bytes = api.get_video_by_tiktok(user_videos[i], custom_device_id=device_id)
+    like_list = [] 
+    for video in user_videos:
+        like_list.append(video['stats']['diggCount'])
+    print(like_list)
+    enumerate_object = enumerate(like_list) 
+    sorted_pairs = sorted(enumerate_object, key=operator.itemgetter(1), reverse=True)
+    index_list = [index for index, element in sorted_pairs]
+    most_liked_videos = [] 
+    for video_index in index_list[0:2]:
+        most_liked_videos.append(user_videos[video_index])
+
+    for i, video in enumerate(most_liked_videos):
+        video_bytes = api.get_video_by_tiktok(video, custom_device_id=device_id)
         with open("video" + str(i) + ".mp4", "wb") as out:
             out.write(video_bytes)
+
+    # json.dumps(api.get_data(url="https://www.tiktok.com/@gordonramsayofficial/video/7028265743003290927?is_copy_url=1&is_from_webapp=v1"))
+    # for video in user_videos:  
+    #     videoJson = api.get_tiktok_by_url()
+    # print(videoJson['itemInfo']['itemStruct']['stats']['diggCount'])
+    # print(videoJson['itemInfo']['itemStruct']['id'])
+
+    # # Below is if the method used if you have the full tiktok object
+    # for i in range(1,4):
+    #     video_bytes = api.get_video_by_tiktok(user_videos[i], custom_device_id=device_id)
+    #     with open("video" + str(i) + ".mp4", "wb") as out:
+    #         out.write(video_bytes)
