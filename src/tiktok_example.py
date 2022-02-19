e3c30df61d9b41c2370a824d92616b97c73fccff
Daniel Raad <daniel_raad@hotmail.co.uk>
2022-02-19 23:00:54 +0000

    as_dict info downloaded and stored for reference

--- src/tiktok_example.py
+++ src/tiktok_example.py
@@ -1,11 +1,17 @@
-from itertools import count
 from TikTokApi import TikTokApi
+import json
 
 
 verify_fp = "verify_kzu8er2q_RX7qGRh6_tLFr_4XZa_BxGD_oaNmpdRX7yxi"
 
 api = TikTokApi(custom_verify_fp=verify_fp)
 
+video_list = []
 for video in api.hashtag(name="motivation").videos(count=5):
-    print(video.as_dict["stats"]["diggCount"])
-    print(video)
+    video_list.append(video)
+
+
+video = video_list[0]
+
+with open("video_info.txt", "w") as convert_file:
+    convert_file.write(json.dumps(video.as_dict))
